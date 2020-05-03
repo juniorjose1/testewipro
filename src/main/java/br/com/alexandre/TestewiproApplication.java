bd6360b585e438cfb0c026359238fd7803570a84
Alexandre <jajrjunior95@gmail.com>
2020-05-03 10:54:56 -0300

    Criando o Projeto

---
+++ src/main/java/br/com/alexandre/TestewiproApplication.java
@@ -0,0 +1,13 @@
+package br.com.alexandre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestewiproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestewiproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alexandre/TestewiproApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alexandre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestewiproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
